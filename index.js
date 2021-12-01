4a4d12ce51db32d1a236138359abb7138819e039
Pavel Catalin <pavelcc11@yahoo.com>
2021-12-01 17:21:11 +0200

    100%

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
 let showNetworks = ()=>{
     let networksContainer = document.querySelector(".share_networks");
-   
+    
     if(networksContainer.style.display === "flex"){
-        networksContainer.style.display =  "none"
+        networksContainer.style.display =  "none";
+        
     }else{
-        networksContainer.style.display =  "flex"
+        networksContainer.style.display =  "flex";
+        
+    }
+    document.querySelector(".share-btn").classList.toggle("global")
+    if(window.innerWidth <= 768){
+        document.querySelector(".share-btn").classList.toggle("mobile")
     }
 }
 document.querySelector(".share-btn").addEventListener("click", showNetworks)
